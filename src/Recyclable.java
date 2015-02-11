ffc172365bb32b39bca4976c0c482a44c3c779d1
chase <chase@bryx.com>
2015-02-11 10:44:30 -0500

    Added comments

--- src/Recyclable.java
+++ src/Recyclable.java
@@ -5,14 +5,26 @@ public abstract class Recyclable {
 
     private boolean safeToRecycle = false;
 
+    /**
+     * Tells the Recycler watching this object that it can recycle this.
+     */
     protected void setToRecycle() {
         safeToRecycle = true;
     }
 
+    /**
+     * @return true if the object is ready to be recycled, false otherwise.
+     */
     public boolean isSafeToRecycle() {
         return safeToRecycle;
     }
 
+    /**
+     * Called by the recycler to let the object know it has been recycled. Supplied with the args
+     * passed to getRecyclable to 're-init' this object.
+     *
+     * @param args The arguments passed to the object to 're-init'
+     */
     public void onRecycle(Object... args) {
         safeToRecycle = false;
     }
